0be49d556f5a37d16fff6e37a29a5a9432b0bc32
chael-in <cofls3511@gmail.com>
2025-09-02 13:28:51 +0900

    style: pretendard 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-black">
-      <h1 className="text-4xl font-bold text-white">Test</h1>
+    <div className="flex min-h-screen items-center justify-center bg-black">
+      <h1 className="text-4xl font-bold text-primary-foreground">Test</h1>
     </div>
   );
 }
