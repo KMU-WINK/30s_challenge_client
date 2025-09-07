e46d4af1a694129a9e4b7166ca87575605a53105
chael-in <cofls3511@gmail.com>
2025-09-07 21:27:50 +0900

    feat: TopBar 컴포넌트 구현

---
+++ src/components/layout/TopBar.tsx
@@ -0,0 +1,16 @@
+import { Icon } from '@iconify/react';
+import type { ReactNode } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function TopBar({ children }: { children: ReactNode }) {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex w-full items-center justify-start gap-4 border-b border-neutral-100 bg-white px-4 py-5">
+      <button type="button" onClick={() => navigate(-1)}>
+        <Icon icon="mdi:chevron-left" className="h-6 w-6"></Icon>
+      </button>
+      {children}
+    </div>
+  );
+}
